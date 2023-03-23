92fa90ddc173fb9f96902567f6c579c81ecffc66
darideveloper <darideveloper@gmail.com>
2023-03-23 14:55:21 -0600

    tutorial done

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+  // Default content for the index route
+  return (
+    <p id="zero-state">
+      This is a demo for React Router.
+      <br />
+      Check out{" "}
+      <a href="https://reactrouter.com">
+        the docs at reactrouter.com
+      </a>
+      .
+    </p>
+  );
+}
